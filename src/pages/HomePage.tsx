11b7a5d03d910e41ad9f1d11eaf54ada39d94b18
MD2SA <albuquerque2158@gmail.com>
2025-06-02 00:28:11 +0100

    added statemetne

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import { IoWarningOutline } from "react-icons/io5";
 import Contacts from "../components/molecules/Contacts";
 import SlideShow from "../components/molecules/SlideShow";
 
@@ -5,6 +6,7 @@ import SlideShow from "../components/molecules/SlideShow";
 export default function HomePage() {
     return (
         <>
+            <h1><IoWarningOutline color="yellow" />SITE IN PROGRESS<IoWarningOutline color="yellow" /></h1>
             <SlideShow />
             <Contacts />
         </>
