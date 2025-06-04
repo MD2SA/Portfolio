035f3ebf6adee0fb1edfcee4d371fee9c5575746
MD2SA <albuquerque2158@gmail.com>
2025-06-04 16:21:56 +0100

    notfoundpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ToolsPage from "./pages/ToolsPage";
 import "./App.css"
+import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
     return (
         <BrowserRouter>
             <div className="background">
-                {/* Optional: Add a subtle gradient overlay */}
                 <div className="overlay"></div>
             </div>
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/tools" element={<ToolsPage />} />
+                <Route path="*" element={<NotFoundPage />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/pages/ToolsPage.tsx
@@ -0,0 +1,10 @@
+
+
+export default function ToolsPage() {
+
+
+
+    return (
+        <div></div>
+    );
+}
