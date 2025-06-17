ef95af7239bd41e03feed98b5b9df358b2eaf23a
MD2SA <albuquerque2158@gmail.com>
2025-06-17 22:51:44 +0100

    slideshow: changed look of icons + ?icon is now github connection and see more became the ?icon

--- src/components/molecules/SlideShow.tsx
+++ src/components/molecules/SlideShow.tsx
@@ -4,14 +4,17 @@ import NOGS from "../../assets/images/NOGS.png";
 import Confidential from "../../assets/images/Confidential.png";
 import { MdOutlineNavigateBefore, MdOutlineNavigateNext } from "react-icons/md";
 import "../../css/SlideShow.css";
-import { FaJava, FaRegQuestionCircle } from "react-icons/fa";
+import { FaJava } from "react-icons/fa";
 import { SiReact, SiJavascript, SiPython, SiDjango, SiSqlite, SiTypescript } from "react-icons/si";
+import { FaGithub } from "react-icons/fa6";
+import { redirect } from "react-router-dom";
 
 const projects = [
     {
         title: "DonkeyKong",
         img: DonkeyKong,
         description: "This is a 2D game inspired by the classic Donkey Kong, developed entirely in Java. The project was originally created as part of the Object-Oriented Programming course, and is currently taking updates for new features",
+        github: "https://github.com/MD2SA/DonkeyKong",
         technologies: [
             { name: "Java", icon: FaJava },
         ],
@@ -20,6 +23,7 @@ const projects = [
         title: "NOGS",
         img: NOGS,
         description: "NOGS is a full-stack typing game platform where users can improve their typing speed, compete against random opponents, and connect with friends or join teams.",
+        github: "https://github.com/MD2SA/NOGS",
         technologies: [
             { name: "Python", icon: SiPython },
             { name: "JavaScript", icon: SiJavascript },
@@ -32,11 +36,13 @@ const projects = [
         title: "Confidential",
         img: Confidential,
         description: "I am currently developing a full-stack mobile app. For this project, I had to scrape multiple files from the internet to populate the information database. Aside from that, the details are confidential.",
+        github: undefined,
         technologies: [
+            { name: "Python", icon: SiPython },
             { name: "TypeScript", icon: SiTypescript },
+            { name: "React", icon: SiReact },
             { name: "Django", icon: SiDjango },
             { name: "SQLite", icon: SiSqlite },
-            { name: "Java", icon: FaJava },
         ],
     }
 ];
@@ -50,6 +56,7 @@ export default function SlideShow() {
     const goForward = () => setCurrentIndex((prev) => (prev + 1) % projects.length);
 
     const currentProject = projects[currentIndex];
+
     const handleDetailClick = () => {
         //for phone
         if (isHovering && !showDetail) setIsHovering(false);
@@ -59,7 +66,10 @@ export default function SlideShow() {
     return (
         <div className="big-container slideshow-container">
             <h2>Projects</h2>
-            <FaRegQuestionCircle className="slideshow-details-button" onClick={handleDetailClick} />
+            <FaGithub
+                onClick={currentProject.github ? () => window.open(currentProject.github, "_blank") : undefined}
+                className="slideshow-details-button"
+            />
             <div
                 onMouseEnter={() => setIsHovering(true)}
                 onMouseLeave={() => setIsHovering(false)}
@@ -70,14 +80,13 @@ export default function SlideShow() {
                         className="slideshow-image"
                         alt={currentProject.title}
                     />
-                    <div className={`slideshow-icons ${showDetail || isHovering ? "visible" : ""}`}>
+                    <div className={`slideshow-icons-container ${showDetail || isHovering ? "visible" : ""}`}>
                         {
                             currentProject.technologies.map((tech, index) => {
-                                const { name, icon: Icon } = tech;
+                                const { icon: Icon } = tech;
                                 return (
-                                    <div key={`tech-${index}`} className="container-techIcon">
+                                    <div key={`tech-${index}`} className="slideshow-icon">
                                         <Icon />
-                                        <span>{name}</span>
                                     </div>
                                 );
                             })
@@ -90,7 +99,7 @@ export default function SlideShow() {
             </div>
             <div className="slideshow-navigation">
                 <MdOutlineNavigateBefore onClick={goBack} className="nav-icon" />
-                <button className="see-more-btn">See More</button>
+                <button onClick={handleDetailClick} className="see-more-btn">See {!showDetail ? "More" : "Less"}</button>
                 <MdOutlineNavigateNext onClick={goForward} className="nav-icon" />
             </div>
         </div>
