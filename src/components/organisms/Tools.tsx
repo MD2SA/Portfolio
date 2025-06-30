532927d36fd98f2e97130e6e557b7312794c691a
MD2SA <albuquerque2158@gmail.com>
2025-06-30 21:47:21 +0100

    removed dimensions logic in tools

--- src/components/organisms/Tools.tsx
+++ src/components/organisms/Tools.tsx
@@ -1,25 +1,19 @@
 import { useState } from "react";
 import InfiniteIcons from "../molecules/InfiniteIcons";
 import IconSection from "../molecules/IconSection";
-import { useMediaQuery } from "../../hooks/useMediaQuery";
 import { sections } from "../../assets/constants";
 
 
 
 export default function Tools() {
     const [showInfinite, setShowInfinite] = useState(true);
-    const isMobile = useMediaQuery('(max-width:840px)');
-
-    const showSectionIcons = isMobile || !showInfinite;
 
     return (
         <div className="icons-container">
-            {showSectionIcons ? <IconSection sections={sections} /> : <InfiniteIcons sections={sections} />}
-            {!isMobile && (
-                <a className="not-selectable" onClick={() => setShowInfinite(!showInfinite)}>
-                    {showInfinite ? 'Show Grid View' : 'Show Infinite Scroll'}
-                </a>
-            )}
+            {!showInfinite ? <IconSection sections={sections} /> : <InfiniteIcons sections={sections} />}
+            <a className="not-selectable" onClick={() => setShowInfinite(!showInfinite)}>
+                {showInfinite ? 'Show Grid View' : 'Show Infinite Scroll'}
+            </a>
         </div>
     );
 }
